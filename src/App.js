341977d23e58b6714e05a039f1da7ab84bd2bacf
chrnk-exe <ivan_kot2001@mail.ru>
2022-05-20 15:42:18 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
+import {Nav} from 'react-bootstrap';
+import classes from './App.module.css';
+import RussiaCovid from './RussiaCovid/RussiaCovid';
+import WorldCovid from './WorldCovid/WorldCovid';
 
 function App() {
-  return (
-    <div className="App">
+  const [world, setWorld] = useState(true);
 
+  const keyHandler = (selectedKey) => {
+    selectedKey === 'Russia' ? setWorld(false) : setWorld(true);
+  };
+
+  return (
+    <div className={classes.App}>
+      <header className={classes.header}>
+        <Nav justify variant='tabs' onSelect={keyHandler}>
+          <Nav.Item/>
+          <Nav.Item>
+            <Nav.Link eventKey="Russia">В России</Nav.Link >
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="World">В Мире</Nav.Link>
+          </Nav.Item>
+          <Nav.Item/>
+        </Nav>
+      </header>
+      <main className={classes.main}>
+        {
+          world ? <WorldCovid/> : <RussiaCovid/>
+        }
+      </main>
+      <footer>
+
+      </footer>
     </div>
   );
 }
---
+++ src/WorldCovid/WorldCovid.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const WorldCovid = () => {
+  return (
+    <div>WorldCovid</div>
+  );
+};
+
+export default WorldCovid;
