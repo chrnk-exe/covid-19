4b6c75f35b3a8529b90af0830e6eb7f9361ebb72
chrnk-exe <ivan_kot2001@mail.ru>
2022-05-21 15:41:54 +0300

    customPeriods

--- src/RussiaCovid/RussiaCovid.jsx
+++ src/RussiaCovid/RussiaCovid.jsx
@@ -10,7 +10,7 @@ import {
   Legend,
 } from 'chart.js';
 import {Bar} from 'react-chartjs-2';
-import {DropdownButton, Dropdown} from 'react-bootstrap'
+import {DropdownButton, Dropdown, Button} from 'react-bootstrap'
 
 function randomInteger(min, max) {
   let rand = min + Math.random() * (max + 1 - min);
@@ -19,6 +19,8 @@ function randomInteger(min, max) {
 
 const Month = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август',
  'сентябрь', 'октябрь', "ноябрь", "декабрь"]
+const currentDate = new Date()
+
 
 ChartJS.register(
     CategoryScale,
@@ -38,19 +40,37 @@ function formatData(date) {
 
 const RussiaCovid = () => {
   const [period, setPeriod] = useState(3)
+  const [customPeriod, setCustomPeriod] = useState({
+    start: new Date(),
+    finish: new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - 14)
+  })
+
   const periods = {
     '1': 'allTime',
     '2': 14,
     '3': 30,
     '4': 90,
     '5': 180,
-    '6': 365,
+    '6': 'customPeriod',
   }
-  const labels = []
-  for(let i = 0; i < periods[period]; i++){
-    let currentDate = new Date()
-    // console.log(currentDate, )
-    labels.push(formatData(new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - i)));
+
+  const [labels, setLabels] = useState(createPeriod(period))
+
+  function createPeriod(start, finish){
+    let dates = []
+    if(finish){
+      const oneDay = 1000 * 60 * 60 * 24; 
+      const diffTime = Math.round(Math.abs(start.getTime() - finish.getTime()) / oneDay + 1)
+      for(let i = 0; i < diffTime; i++){
+        dates.push(formatData(new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - i)));
+      }
+      return dates
+    } else if(start) {
+      for(let i = 0; i < periods[start]; i++){
+        dates.push(formatData(new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - i)));
+      }
+      return dates
+    }
   }
 
   const data = {
@@ -58,7 +78,7 @@ const RussiaCovid = () => {
     datasets: [
       {
         label: 'Covid in Russia',
-        data: labels.map(() => randomInteger(0, 1000)),
+        data: labels.map(() => randomInteger(0, 10000)),
         backgroundColor: 'rgba(53, 162, 235, 0.5)',
       },
     ],
@@ -82,12 +102,29 @@ const RussiaCovid = () => {
     },
   };
 
+  const customPeriodHandler = event => {
+    event.target.name === 'start' 
+    ? setCustomPeriod({...customPeriod, start: new Date(event.target.value)}) 
+    : setCustomPeriod({...customPeriod, finish: new Date(event.target.value)}) 
+  }
+
+  const changeCustomPeriodHandler = event => {
+    const start = new Date(customPeriod.start)
+    const finish = new Date(customPeriod.finish)
+    setLabels(createPeriod(start, finish))
+  }
+
+  const changePeriodHandler = eventKey => {
+    setPeriod(eventKey)
+    setLabels(createPeriod(eventKey))
+  }
+
   return (
     <div className={classes.main}>
       <Bar options={options} data={data}></Bar>
       <div className={classes.options}>
         <div className={classes.period}>
-          <DropdownButton onSelect={(eKey) => setPeriod(eKey)} variant='primary' title='Период'>
+          <DropdownButton onSelect={changePeriodHandler} variant='primary' title='Период'>
             <Dropdown.Item eventKey='1' active={period === '1'}>Всё время</Dropdown.Item>
             <Dropdown.Item eventKey='2' active={period === '2'}>2 Недели</Dropdown.Item>
             <Dropdown.Item eventKey='3' active={period === '3'}>Месяц</Dropdown.Item>
@@ -96,6 +133,14 @@ const RussiaCovid = () => {
             <Dropdown.Divider />
             <Dropdown.Item eventKey='6' active={period === '6'}>Произвольный период</Dropdown.Item>
           </DropdownButton>
+          {
+            period === '6' 
+            ? <div className={classes.customPeriod}>
+                From <input name='start' onChange={customPeriodHandler} type='date'/> to <input name='finish' onChange={customPeriodHandler} type='date'/> 
+                <Button onClick={changeCustomPeriodHandler} variant='success'>OK</Button>
+              </div>
+            : null
+          }
         </div>
       </div>
     </div>
