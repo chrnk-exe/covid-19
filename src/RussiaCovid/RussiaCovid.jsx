59d28172ed782fa14a2ce48f9afd773645ae2fe3
chrnk-exe <ivan_kot2001@mail.ru>
2022-05-22 21:09:21 +0300

    getData

--- src/RussiaCovid/RussiaCovid.jsx
+++ src/RussiaCovid/RussiaCovid.jsx
@@ -11,7 +11,7 @@ import {
 } from 'chart.js';
 import {Bar} from 'react-chartjs-2';
 import {DropdownButton, Dropdown, Button} from 'react-bootstrap'
-import {createPeriod, currentDate, Month, formatData, randomInteger} from './dataApi'
+import {createPeriod, currentDate, randomInteger, getData} from './dataApi'
 
 ChartJS.register(
     CategoryScale,
