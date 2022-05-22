e34294ec872ae9d4a2f99a1b548d47912de85323
chrnk-exe <ivan_kot2001@mail.ru>
2022-05-22 16:08:07 +0300

    buttons

--- src/RussiaCovid/RussiaCovid.jsx
+++ src/RussiaCovid/RussiaCovid.jsx
@@ -37,13 +37,28 @@ function formatData(date) {
   return [date.getDate(), Month[date.getMonth()]].join(' ')
 }
 
+const AllCases = 'Все случаи заражения'
+const NewCases = 'Новые случаи заражения'
+const LethalCases = 'Летальные исходы'
+const RecoveryCases = 'Случаи выздоровления'
+const Diff = 'Нагрузка на систему здравоохранения'
+
+const colors = {
+  AllCases: "#FE5767",
+  NewCases: "#FFE92F",
+  LethalCases: '#89929A',
+  RecoveryCases: '#19E154',
+}
 
 const RussiaCovid = () => {
+
   const [period, setPeriod] = useState(3)
   const [customPeriod, setCustomPeriod] = useState({
     start: new Date(),
     finish: new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - 14)
   })
+  const [parameters, setParameters] = useState([NewCases])
+
 
   const periods = {
     '1': 'allTime',
@@ -55,6 +70,16 @@ const RussiaCovid = () => {
   }
 
   const [labels, setLabels] = useState(createPeriod(period))
+  const [data, setData] = useState({
+    labels,
+    datasets: [
+      {
+        label: 'Новые случаи заражения',
+        data: labels.map(() => randomInteger(0, 10000)),
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  })
 
   function createPeriod(start, finish){
     let dates = []
@@ -62,7 +87,7 @@ const RussiaCovid = () => {
       const oneDay = 1000 * 60 * 60 * 24; 
       const diffTime = Math.round(Math.abs(start.getTime() - finish.getTime()) / oneDay + 1)
       for(let i = 0; i < diffTime; i++){
-        dates.push(formatData(new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - i)));
+        dates.push(formatData(new Date(finish.getFullYear(), finish.getMonth(), finish.getDate() - i)));
       }
       return dates
     } else if(start) {
@@ -72,17 +97,6 @@ const RussiaCovid = () => {
       return dates
     }
   }
-
-  const data = {
-    labels,
-    datasets: [
-      {
-        label: 'Covid in Russia',
-        data: labels.map(() => randomInteger(0, 10000)),
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      },
-    ],
-  };
 
   const options = {
     responsive: true,
@@ -97,7 +111,7 @@ const RussiaCovid = () => {
       },
       title: {
         display: true,
-        text: 'Chart.js Bar Chart',
+        text: 'Covid-19 in Russia',
       },
     },
   };
@@ -119,6 +133,16 @@ const RussiaCovid = () => {
     setLabels(createPeriod(eventKey))
   }
 
+  const parametersHandler = event => {
+    if(!parameters.includes(event.target.value) && !(parameters.length == 1 && parameters[0] === Diff))setParameters([...parameters, event.target.value])
+    else setParameters([event.target.value])
+    if(event.target.value === Diff)setParameters([Diff])
+  }
+
+  const deleteParameterHandler = event => {
+    setParameters(parameters.filter(param => param != event.target.value))
+  }
+
   return (
     <div className={classes.main}>
       <Bar options={options} data={data}></Bar>
@@ -136,13 +160,38 @@ const RussiaCovid = () => {
           {
             period === '6' 
             ? <div className={classes.customPeriod}>
-                From <input name='start' onChange={customPeriodHandler} type='date'/> to <input name='finish' onChange={customPeriodHandler} type='date'/> 
+                From <input name='start' onChange={customPeriodHandler} type='date'/> to <input name='finish' onChange={customPeriodHandler} type='date'/> {' '} 
                 <Button onClick={changeCustomPeriodHandler} variant='success'>OK</Button>
               </div>
             : null
           }
         </div>
+        <div className={classes.parameters}>
+          <div className={classes.parametersToAdd}>
+            <Button onClick={parametersHandler} value={AllCases} className={classes.parametersBtn} variant='danger'>Все случаи заболевания</Button>
+            <Button onClick={parametersHandler} value={NewCases} className={classes.parametersBtn} variant='warning'>Новые случаи заболевания</Button>
+            <Button onClick={parametersHandler} value={LethalCases} className={classes.parametersBtn} variant='secondary'>Летальные исходы</Button>
+            <Button onClick={parametersHandler} value={RecoveryCases} className={classes.parametersBtn} variant='success'>Случаи выздоровления</Button>
+            <Button onClick={parametersHandler} value={Diff} className={classes.parametersBtn} variant='info'>Разница между заболевшими и выздоровевшими</Button>
+          </div>
+          <div className={classes.addedParameters}>
+            {
+              parameters.map((param, index) => (
+                <div key={index}>
+                  <Button 
+                  value={param} 
+                  onClick={deleteParameterHandler} 
+                  className={classes.parametersBtn} 
+                  variant='outline-dark'>
+                    {param} ×
+                  </Button>
+                </div>
+              ))
+            }
+          </div>
+        </div>
       </div>
+      
     </div>
   );
 };
